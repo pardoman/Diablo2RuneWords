e25831d04d47693c16c4db9c181fd3fb4604e53c
pardoman <pardo.medina@gmail.com>
2018-11-21 23:01:52 -0800

    Add remove rune from filter click event

--- src/ui/filter.js
+++ src/ui/filter.js
@@ -7,6 +7,7 @@ var _filterDiv;
 export function initFilter(div) {
 
     _filterDiv = document.createElement('div');
+    _filterDiv.addEventListener('click', onRuneClick);
     renderFilter();
     div.appendChild(_filterDiv);
 
@@ -18,4 +19,14 @@ function renderFilter() {
     const plainObject = filterData.getPlainObject();
     _filterDiv.innerHTML = Mustache.render(TEMPLATE, plainObject);
     _filterDiv.className = 'filter';
+}
+
+function onRuneClick() {
+    if (!event.target.classList.contains('rune')) {
+        return;
+    }
+
+    var runeName = event.target.textContent;
+    let filterData = getFilterData();
+    filterData.removeRune(runeName);
 }
