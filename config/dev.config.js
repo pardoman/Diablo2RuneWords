6df578c390b0a806d7d164e820e6295f52bf8107
pardoman <pardo.medina@gmail.com>
2018-11-21 17:20:32 -0800

    Fix `npm run dev`

--- config/dev.config.js
+++ config/dev.config.js
@@ -4,5 +4,8 @@ const common = require('./common.config');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: common.output.path
+  }
 });
 
