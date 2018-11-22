b0eee74beb6945b3818ea33a14b5c6872b042a2a
pardoman <pardo.medina@gmail.com>
2018-11-21 17:23:33 -0800

    Add generated _build/index.html
    Using webpack plugin

--- config/common.config.js
+++ config/common.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/entry.js',
     output: {
         path: path.resolve(__dirname, "../_build"),
         filename: 'bundle.js',
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
 }
